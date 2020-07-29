d3e51b2fb2f25d879c120345bdbe77714d4e6147
David Nguyen <david.th.nguy@gmail.com>
2020-07-29 16:09:36 -0500

    functions to simulate pop dynamics with process and obs error

---
+++ noisy_pop_scripts.R
@@ -0,0 +1,62 @@
+# simulation code for populations with process and observation error
+library(ggplot2)
+
+sim_pop <- function (xinit,     # initial log population size
+                        lambda, # population growth rate
+                        b,      # density dependence
+                        phi,    # process noise autocorrelation [-1,1]
+                        sd_proc, # process noise: N(0, sd_proc)
+                        sd_obs,  # observation noise: N(0, sd_obs)
+                        tfinal,
+                        nsim) {
+  
+  # init storage list for all simulation
+  sim_list <- vector("list", length = nsim)
+  
+  for (i in seq_along(sim_list)) {
+    # init storage for current simulation
+    log_x <- vector("numeric", length = tfinal)
+    log_y <- vector("numeric", length = tfinal)
+    
+    proc_error <- rnorm(n = 1, mean = 0, sd = sd_proc)
+    
+    log_x[1] <- xinit  
+    log_y[1] <- xinit + rnorm(n = 1, mean = 0, sd = sd_obs) 
+    
+    # sim population dynamics
+    for (j in 2:tfinal) {
+      proc_error <- phi * proc_error + rnorm(n = 1, mean = 0, sd = sd_proc)
+      log_x[j] <- lambda + b * log_x[j-1] + proc_error
+      log_y[j] <- log_x[j] + rnorm(n = 1, mean = 0, sd = sd_proc)
+    }
+    # save current simulation
+    sim_list[[i]] <- (tidyr::tibble(time = 1:tfinal,
+                                    log_x = log_x,
+                                    log_y = log_y))
+  } 
+  
+  return(dplyr::bind_rows(sim_list, .id = "sim"))
+}
+
+set_params_expand <- function (xinit = c(), lambda = c(), b = c(), phi = c(), sd_proc = c(), sd_obs = c()) {
+  return(tidyr::expand_grid(xinit = xinit, lambda = lambda, b = b, phi = phi, sd_proc = sd_proc, sd_obs = sd_obs))
+}
+
+set_params <- function (xinit, lambda, b, phi, sd_proc, sd_obs) {
+  return(tidyr::tibble(xinit = xinit, lambda = lambda, b = b, phi = phi, sd_proc = sd_proc, sd_obs = sd_obs))
+}
+
+multi_sim_pop <- function (params, tfinal, nsim) {
+  sim_list <- vector("list", length = nrow(params))
+  
+  for ( i in 1:nrow(params)) {
+    sim_list[[i]] <- with(params[i,],
+                          sim_pop(xinit = xinit, lambda = lambda, b = b, phi = phi, sd_proc = sd_proc, sd_obs = sd_obs,
+                             tfinal = tfinal, nsim = nsim)
+                            )
+    sim_list[[i]]$par <- rep(paste(round(pars[i,], 2), collapse = ", "), times = nrow(sim_list[[i]]))
+  }
+  return(dplyr::bind_rows(sim_list))
+}
+
+       
