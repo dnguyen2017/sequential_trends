cdc6aa5fb92313d169fa24d3874623e6d04db85c
David Nguyen <david.th.nguy@gmail.com>
2020-08-05 17:29:13 -0500

    add sprt functions

--- trend_detection_functions.R
+++ trend_detection_functions.R
@@ -30,6 +30,18 @@ multi_sim <- function(nsim, x0, t, trend, sd) {
 
 ### analysis functions
 
+# apply FUNC cumulatively when NA are present
+# https://stackoverflow.com/questions/25576358/calculate-cumsum-while-ignoring-na-values/25576972
+cumSkipNA <- function(x, FUNC)
+{
+  d <- deparse(substitute(FUNC))
+  funs <- c("max", "min", "prod", "sum")
+  stopifnot(is.vector(x), is.numeric(x), d %in% funs)
+  FUNC <- match.fun(paste0("cum", d))
+  x[!is.na(x)] <- FUNC(x[!is.na(x)])
+  x
+}
+
 # running calculation for mle estimates of normal R.V.s
 
 run_stat <- function(x) {
@@ -53,7 +65,8 @@ run_stat <- function(x) {
   }
   
   # return: id = observation number, obs = observation value, mu_mle, sd_mle, sd_corrected
-  return(tidyr::tibble(x, mu_mle = m_, sd_mle, sd_corrected = sd_)) # %>% tibble::rowid_to_column(var = "id"))
+  return(tidyr::tibble(diff_1 = x, mu_mle = m_, sd_mle, sd_corrected = sd_)) # %>% tibble::rowid_to_column(var = "id"))
+  #return(tidyr::tibble(diff_1 = c(NA,x), mu_mle = c(NA,m_), sd_mle = c(NA,sd_mle), sd_corrected = c(NA,sd_) )) # %>% tibble::rowid_to_column(var = "id"))
 }
 
 #out %>% ggplot() + geom_line(aes(x = id, y = mu_mle))
@@ -85,4 +98,57 @@ seq_lm <- function (data, nsim) {
   }
   
   return(bind_rows(lm_list))
+}
+
+# one sided sprt calculation
+objfn <- function(trend, x1, x0, sd) {
+  # likelihood( x1 | x0, r, dispersion )
+  dnorm(x = x1, mean = x0 + trend, sd = sd)
+}
+
+
+#  find restricted MLE under h_0 or h_1
+likelihood <- function (x_prev, x_now, sd, interval = c(), guess = 1.5) {
+  opt <- optim(par = guess, fn = objfn, control = list(fnscale = -1), 
+               method = "Brent", lower = interval[1], upper = interval[2],
+               x0 = x_prev, x1 = x_now, sd = sd)
+  return(opt$value)
+}
+
+# sequential test functions
+calc_sr <- function(data, accept_reg = c(0,0), reject_reg = c(0, 5)) {
+  
+  # initialize empty columns for likelihood calculations
+  data$lik0 <- NA
+  data$lik1 <- NA
+  
+  # parameter space
+  theta0 <- accept_reg
+  theta1 <- reject_reg
+  
+  # use midpoint of interval as init guess for optim (Brent's method)
+  guess0 <- accept_reg[1] + (accept_reg[2] - accept_reg[1]) / 2 
+  guess1 <- reject_reg[1] + (reject_reg[2] - reject_reg[1]) / 2 
+  
+  sim_number <- 1
+  for (i in 2:nrow(data)) {
+    
+    if (data[i, "simulation"] == sim_number + 1) {
+      # skip calculation of likelihood for the first observation in each simluation
+      sim_number <- sim_number + 1
+      next
+    }
+    
+    # observed states and known pars
+    x0 <- unlist(data[i-1, "pop"])
+    x1 <- unlist(data[i, "pop"])
+    sd <- unlist(data[i, "sd"])
+    
+    # likelihood calculation
+    data[i,"lik0"] <- dnorm(x = x1, mean = x0, sd = sd) # H0: x_t+1 ~ N(x_t, sd)
+    data[i,"lik1"] <- likelihood(x_prev = x0, x_now = x1, sd = sd,  # p(x_now | x_prev, disp, theta \in Theta_1)
+                                 interval = theta1, guess = guess1)
+  }
+  
+  return(data)
 }
